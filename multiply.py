e59fea6773cffac1a2d8041606016c2215a1ddd8
lTJShowerHandel <blanesantilli@gmail.com>
2025-02-20 13:34:23 -0700

    Created Multiply Function
    Takes in two numbers and multiplies them

---
+++ multiply.py
@@ -0,0 +1,10 @@
+# Blane 
+# Feb 20 2025
+# take 2 numbers and multiply them 
+
+def multiplyTwoNumbers(iNum1,iNum2):
+    results = iNum1 * iNum2
+    return results
+
+
+product = multiplyTwoNumbers(2,3)
